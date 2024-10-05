ec5a220f4df05ef7dd9240015eba40e961ae210d
Pavel Balusek <mr.p.balusek@gmail.com>
2024-10-05 21:56:34 +0200

    Rename project, Add architecture libraries

---
+++ FormsIW5.BL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FormsIW5.BL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ FormsIW5.DAL/FormsIW5DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FormsIW5.DAL
+{
+    public class FormsIW5DbContext : DbContext
+    {
+        public FormsIW5DbContext(DbContextOptions<FormsIW5DbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
