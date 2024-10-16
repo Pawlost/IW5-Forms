bf82339d9ea913d5bdda28e9dadb8a6e75abe322
Pavel Balusek <mr.p.balusek@gmail.com>
2024-10-17 01:29:36 +0200

    Add DAL installer, Remade api project, Install necessary libraries

--- FormsIW5.DAL/Factories/FormsIW5DbContextFactory.cs
+++ FormsIW5.DAL/Factories/FormsIW5DbContextFactory.cs
@@ -4,18 +4,18 @@ using Microsoft.Extensions.Configuration;
 
 namespace FormsIW5.DAL.Factories;
 
-    public class FormsIW5DbContextFactory : IDesignTimeDbContextFactory<FormsIW5DbContext>
+public class FormsIW5DbContextFactory : IDesignTimeDbContextFactory<FormsIW5DbContext>
+{
+    public FormsIW5DbContext CreateDbContext(string[] args)
     {
-        public FormsIW5DbContext CreateDbContext(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddUserSecrets<FormsIW5DbContextFactory>(optional: true)
-                .Build();
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<FormsIW5DbContextFactory>(optional: true)
+            .Build();
 
-            var optionsBuilder = new DbContextOptionsBuilder<FormsIW5DbContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-            return new FormsIW5DbContext(optionsBuilder.Options);
-        }
+        var optionsBuilder = new DbContextOptionsBuilder<FormsIW5DbContext>();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        return new FormsIW5DbContext(optionsBuilder.Options);
     }
+}   
 
 
