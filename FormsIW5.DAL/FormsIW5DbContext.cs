051a0fbeff0c281c66c27848eee7dd66ff431e42
Pavel Balusek <mr.p.balusek@gmail.com>
2024-10-16 23:36:30 +0200

    Add basic DB entities, Add Initial EF migration

--- FormsIW5.DAL/FormsIW5DbContext.cs
+++ FormsIW5.DAL/FormsIW5DbContext.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FormsIW5.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace FormsIW5.DAL
+namespace FormsIW5.DAL;
+
+public class FormsIW5DbContext(DbContextOptions<FormsIW5DbContext> options) : DbContext(options)
 {
-    public class FormsIW5DbContext : DbContext
-    {
-        public FormsIW5DbContext(DbContextOptions<FormsIW5DbContext> options) : base(options)
-        {
-        }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<QuestionEntity> Questions { get; set; }
+    public DbSet<FormEntity> Forms { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserEntity>()
+            .HasMany(userEntity => userEntity.Forms)
+            .WithOne(formEntity => formEntity.User)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<FormEntity>()
+            .HasMany(formEntity => formEntity.Questions)
+            .WithOne(questionEntity => questionEntity.Form)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
