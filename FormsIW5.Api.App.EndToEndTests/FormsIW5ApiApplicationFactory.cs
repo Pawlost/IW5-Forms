d936ebf8022c6fc77e9176584254524a6e975f57
Pavel Balusek <mr.p.balusek@gmail.com>
2024-10-23 00:22:17 +0200

    Add end to end test experiment

---
+++ FormsIW5.Api.App.EndToEndTests/FormsIW5ApiApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using FormsIW5.Api.DAL;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormsIW5.Api.App.EndToEndTests;
+
+public class FormsIW5ApiApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(collection =>
+        {
+            var controllerAssemblyName = typeof(Program).Assembly.FullName;
+            collection.AddMvc().AddApplicationPart(Assembly.Load(controllerAssemblyName));
+
+            var test = Environment.GetEnvironmentVariable("LMAO");
+
+            Console.WriteLine(test);
+          /*  collection.AddDbContext<FormsIW5DbContext>(options =>
+            options.UseSqlServer(connectionString));*/
+        });
+
+        return base.CreateHost(builder);
+    }
+}
