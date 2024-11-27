af266cb5237fe053d1b9dbbf478966e59789e3ac
Pavel Balusek <mr.p.balusek@gmail.com>
2024-11-27 14:42:02 +0100

    Add Identity provider

---
+++ FormsIW5.IdentityProvider.App/TestUsers.cs
@@ -0,0 +1,25 @@
+﻿using Duende.IdentityServer.Test;
+
+namespace FormsIW5.IdentityProvider.App
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users { 
+            get {
+                var address = new {
+                    street_address = "Hack"
+                };
+                return [
+                    new TestUser{
+                        SubjectId = "1",
+                        Username="User",
+                        Password="password",
+                        Claims={ 
+                        
+                        }
+                    }
+                   ];
+            }
+        }
+    }
+}
